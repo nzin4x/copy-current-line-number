a3c8fe4c2d8f16dbc6395a6b808952c405f76866
nzin4x <nzin4x@gmail.com>
2020-10-09 16:11:41 +0900

    clean logs

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "copy-current-line-number" is now active!');
+	// console.log('Congratulations, your extension "copy-current-line-number" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -18,7 +18,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from copy-current-line-number!');
+		// vscode.window.showInformationMessage('Hello World from copy-current-line-number!');
 
 		// get cursor line number, zero index so add 1
 		
@@ -31,7 +31,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		
 		let cursorLinePos = editor.selection.active.line + 1;
-		console.log("current line : " + cursorLinePos);
+		// console.log("current line : " + cursorLinePos);
 
         copypaste.copy(cursorLinePos, res => {
             if(res != null) {
