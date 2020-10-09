6f40e601d68ad0dbde45793fb73dac418a49d84b
nzin4x <nzin4x@gmail.com>
2020-10-09 16:01:51 +0900

    initial files for copy current line to clipboard

---
+++ src/extension.ts
@@ -0,0 +1,49 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import copypaste = require('copy-paste');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "copy-current-line-number" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('copy-current-line-number.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from copy-current-line-number!');
+
+		// get cursor line number, zero index so add 1
+		
+        // Get the current text editor
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            // notify the user nothing can be done without active editor
+            vscode.window.showErrorMessage('No active editor found, cannot do anything.');
+            return;
+		}
+		
+		let cursorLinePos = editor.selection.active.line + 1;
+		console.log("current line : " + cursorLinePos);
+
+        copypaste.copy(cursorLinePos, res => {
+            if(res != null) {
+                // something went wrong...
+                vscode.window.showErrorMessage('Could not copy: '+res);
+            }
+		});
+
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
